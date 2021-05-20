33d38f42e3ad130da1c841fb2849723620858f0b
Munawaer <munawaer.ainiwaer@gmail.com>
2021-05-20 21:09:16 +0000

    Complete initial game.

--- game.py
+++ game.py
@@ -2,19 +2,20 @@
 
 from random import choice
 
-# Put your code here
+
 def get_user_name():
     """Greet player and return their name input."""
     return input("Howdy, what's your name? > ")
 
+
 def guessing_game(name):
     """Run game where user guesses a randomly generated number."""
 
-    guess_number = choice(range(1,100))
+    guess_number = choice(range(1, 100))
 
     guess_counter = 0
 
-    print(f"{name}, I'm thinking of a number between 1 and 100. \n Try to guess my number.")
+    print(f"{name}, I'm thinking of a number between 1 and 100. \nTry to guess my number.")
 
     while True:
         player_guess = int(input("Your guess? > "))
@@ -26,7 +27,9 @@ def guessing_game(name):
 
         elif player_guess > guess_number:
             print("Your guess is too high, try again.")
-        
+
         elif player_guess < guess_number:
             print("Your guess is too low, try again")
 
+
+guessing_game(name=get_user_name())
