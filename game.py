c4f458ee5a9b9b31f2832401285ab3e26e223a69
Munawaer <munawaer.ainiwaer@gmail.com>
2021-05-20 20:50:14 +0000

    Write function to greet player

--- game.py
+++ game.py
@@ -1,3 +1,9 @@
 """A number-guessing game."""
 
+from random import choice
+
 # Put your code here
+def get_user_name():
+    """Greet player and return their name input."""
+    return input("Howdy, what's your name? >")
+
