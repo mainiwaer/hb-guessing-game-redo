75fd819561f8a7894fafd1e3133f4db45b142235
Munawaer <munawaer.ainiwaer@gmail.com>
2021-05-20 20:51:37 +0000

    start guessing_game function: random # generation, query user

--- game.py
+++ game.py
@@ -7,3 +7,12 @@ def get_user_name():
     """Greet player and return their name input."""
     return input("Howdy, what's your name? >")
 
+def guessing_game(name):
+    """Run game where user guesses a randomly generated number."""
+
+    guess_number = choice(range(1,100))
+
+    print(f"{name}, I'm thinking of a number between 1 and 100. \n Try to guess my number.")
+
+    while True:
+        input
