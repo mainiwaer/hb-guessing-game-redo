a3e911f9c245e21c0ee29236a415325d071eb969
Munawaer <munawaer.ainiwaer@gmail.com>
2021-05-20 20:55:19 +0000

    Create win condition for guessing game, add guess counter

--- game.py
+++ game.py
@@ -5,14 +5,22 @@ from random import choice
 # Put your code here
 def get_user_name():
     """Greet player and return their name input."""
-    return input("Howdy, what's your name? >")
+    return input("Howdy, what's your name? > ")
 
 def guessing_game(name):
     """Run game where user guesses a randomly generated number."""
 
     guess_number = choice(range(1,100))
 
+    guess_counter = 0
+
     print(f"{name}, I'm thinking of a number between 1 and 100. \n Try to guess my number.")
 
     while True:
-        input
+        player_guess = input("Your guess? > ")
+        guess_counter += 1
+
+        if player_guess == guess_number:
+            print(f"Congrats {name}! You found my number in {guess_counter} tries!")
+            break
+
