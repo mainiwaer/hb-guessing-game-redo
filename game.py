195cc4ec8dc0a053d9653f8bcebc91a639feba32
Munawaer <munawaer.ainiwaer@gmail.com>
2021-05-21 00:00:25 +0000

    Add best score record

--- game.py
+++ game.py
@@ -8,17 +8,17 @@ def get_user_name():
     return input("Howdy, what's your name? > ")
 
 
-def replay_game(name):
+def replay_game(name, best_score):
     """Prompts user to play again after completing a round of the game."""
     replay_prompt = input("Would you like to play again? (y/n) > ").lower()
     if replay_prompt == "y" or replay_prompt == "yes":
-        guessing_game(name)
+        guessing_game(name, best_score)
 
     else:
-        print("Bye {name}!")
+        print(f"Bye {name}!")
 
 
-def guessing_game(name):
+def guessing_game(name, best_score=None):
     """Run game where user guesses a randomly generated number."""
 
     guess_number = choice(range(1, 100))
@@ -41,7 +41,9 @@ def guessing_game(name):
             guess_counter += 1
 
             if player_guess == guess_number:
-                print(f"Congrats {name}! You found my number in {guess_counter} tries!")
+                if best_score is None or guess_counter < best_score:
+                    best_score = guess_counter
+                print(f"Congrats {name}! You found my number in {guess_counter} tries! \n Best Score: {best_score}")
                 break
 
             elif player_guess > guess_number:
@@ -50,7 +52,11 @@ def guessing_game(name):
             elif player_guess < guess_number:
                 print("Your guess is too low, try again")
 
-    replay_game(name)
+        if guess_counter >= 10:
+            print("Maximum guesses reached!")
+            break
+
+    replay_game(name, best_score)
 
 
 guessing_game(name=get_user_name())
